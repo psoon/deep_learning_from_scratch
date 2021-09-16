9e5d9e4d37d9a36b8b646b60c0ac4ab91cd40998
psoon <bestkcs1234@gachon.ac.kr>
2021-09-16 22:01:06 +0900

    asdf

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
-# This is a sample Python script.
+import sys, os
+sys.path.append(os.pardir)
+import numpy as np
+from dataset.mnist import  load_mnist
+from ch05.two_layer_net import TwoLayerNet
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
+iters_num = 10000
+train_size = x_train.shape[0]
+batch_size = 100
+learning_rate = 0.01
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+train_loss_list = []
+train_acc_list = []
+test_acc_list = []
 
+iter_per_epoch = max(train_size / batch_size, 1)
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    grad = network.gradient(x_batch, t_batch)
+
+    for key in ('W1', 'b1', 'W2', 'b2'):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+    if i % iter_per_epoch == 0:
+        train_acc = network.accuracy(x_train, t_train)
+        test_acc = network.accuracy(x_test, t_test)
+        train_acc_list.append(train_acc)
+        test_acc_list.append(test_acc)
+        print(train_acc, test_acc)
